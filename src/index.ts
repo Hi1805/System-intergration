4b806b31575cb1bad48c0091b02d0556cf5ac179
john1805 <ripker1805@gmail.com>
2022-03-26 12:42:05 +0700

    add limit request

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import apiRouter from './routes';
 import dotenv from 'dotenv';
+import multer from 'multer';
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-import multer from 'multer';
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 const upload = multer();
 const port = process.env.PORT || 5000;
 
