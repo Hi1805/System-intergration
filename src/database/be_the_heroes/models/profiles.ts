e6a908507d5d2875ac8f39824952b3daa3d5b370
john1805 <ripker1805@gmail.com>
2022-04-09 12:19:38 +0700

    Add addtribute uid of profile

--- src/database/be_the_heroes/models/profiles.ts
+++ src/database/be_the_heroes/models/profiles.ts
@@ -3,6 +3,8 @@ import { DataTypes, Model, Optional } from 'sequelize';
 import type { users, usersId } from './users';
 
 export interface profilesAttributes {
+  user_id: number;
+  uid: string;
   first_name: string;
   last_name: string;
   middle_name?: string;
@@ -11,15 +13,28 @@ export interface profilesAttributes {
   status?: 'private' | 'public';
   avatar?: string;
   cover_image?: string;
-  user_id: number;
 }
 
-export type profilesPk = "user_id";
+export type profilesPk = 'user_id';
 export type profilesId = profiles[profilesPk];
-export type profilesOptionalAttributes = "middle_name" | "date_of_birth" | "phone" | "status" | "avatar" | "cover_image";
-export type profilesCreationAttributes = Optional<profilesAttributes, profilesOptionalAttributes>;
+export type profilesOptionalAttributes =
+  | 'middle_name'
+  | 'date_of_birth'
+  | 'phone'
+  | 'status'
+  | 'avatar'
+  | 'cover_image';
+export type profilesCreationAttributes = Optional<
+  profilesAttributes,
+  profilesOptionalAttributes
+>;
 
-export class profiles extends Model<profilesAttributes, profilesCreationAttributes> implements profilesAttributes {
+export class profiles
+  extends Model<profilesAttributes, profilesCreationAttributes>
+  implements profilesAttributes
+{
+  user_id!: number;
+  uid!: string;
   first_name!: string;
   last_name!: string;
   middle_name?: string;
@@ -28,7 +43,6 @@ export class profiles extends Model<profilesAttributes, profilesCreationAttribut
   status?: 'private' | 'public';
   avatar?: string;
   cover_image?: string;
-  user_id!: number;
 
   // profiles belongsTo users via user_id
   user!: users;
@@ -37,63 +51,68 @@ export class profiles extends Model<profilesAttributes, profilesCreationAttribut
   createUser!: Sequelize.BelongsToCreateAssociationMixin<users>;
 
   static initModel(sequelize: Sequelize.Sequelize): typeof profiles {
-    return profiles.init({
-    first_name: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    last_name: {
-      type: DataTypes.STRING(100),
-      allowNull: false
-    },
-    middle_name: {
-      type: DataTypes.STRING(100),
-      allowNull: true
-    },
-    date_of_birth: {
-      type: DataTypes.DATEONLY,
-      allowNull: true
-    },
-    phone: {
-      type: DataTypes.STRING(12),
-      allowNull: true
-    },
-    status: {
-      type: DataTypes.ENUM('private','public'),
-      allowNull: true,
-      defaultValue: "public"
-    },
-    avatar: {
-      type: DataTypes.STRING(100),
-      allowNull: true
-    },
-    cover_image: {
-      type: DataTypes.STRING(100),
-      allowNull: true
-    },
-    user_id: {
-      type: DataTypes.BIGINT,
-      allowNull: false,
-      primaryKey: true,
-      references: {
-        model: 'users',
-        key: 'user_id'
-      }
-    }
-  }, {
-    sequelize,
-    tableName: 'profiles',
-    timestamps: false,
-    indexes: [
+    return profiles.init(
       {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "user_id" },
-        ]
+        uid: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        first_name: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+        },
+        last_name: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+        },
+        middle_name: {
+          type: DataTypes.STRING(100),
+          allowNull: true,
+        },
+        date_of_birth: {
+          type: DataTypes.DATEONLY,
+          allowNull: true,
+        },
+        phone: {
+          type: DataTypes.STRING(12),
+          allowNull: true,
+        },
+        status: {
+          type: DataTypes.ENUM('private', 'public'),
+          allowNull: true,
+          defaultValue: 'public',
+        },
+        avatar: {
+          type: DataTypes.STRING(100),
+          allowNull: true,
+        },
+        cover_image: {
+          type: DataTypes.STRING(100),
+          allowNull: true,
+        },
+        user_id: {
+          type: DataTypes.BIGINT,
+          allowNull: false,
+          primaryKey: true,
+          references: {
+            model: 'users',
+            key: 'user_id',
+          },
+        },
       },
-    ]
-  });
+      {
+        sequelize,
+        tableName: 'profiles',
+        timestamps: false,
+        indexes: [
+          {
+            name: 'PRIMARY',
+            unique: true,
+            using: 'BTREE',
+            fields: [{ name: 'user_id' }],
+          },
+        ],
+      }
+    );
   }
 }
